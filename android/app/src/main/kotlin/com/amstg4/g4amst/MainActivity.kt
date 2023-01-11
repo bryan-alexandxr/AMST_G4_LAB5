93c3f65e832b3102a503146972e6e5e872e5445d
bryan-alexandxr <balopez@espol.edu.ec>
2023-01-11 16:45:44 -0500

    Commit de todos los resultados logrados en el laboratorio. Faltan los desafios

---
+++ android/app/src/main/kotlin/com/amstg4/g4amst/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.amstg4.g4amst
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:g4amst/ui/exampleapp.dart';
+
+void main() {
+  runApp(ExampleApp());
+}
